135b1c8e56510c1f51221a1eea81fdc6b063bb45
Filip Korus <filipkorus123@gmail.com>
2024-06-02 16:53:41 +0200

    evaluate model

--- model_evaluation.py
+++ model_evaluation.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.utils import to_categorical
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
 
+OUTPUT_FILE = 'classification_report.txt'
 
 labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 le = LabelEncoder()
@@ -17,34 +18,17 @@ models_folder = 'models'
 
 # Słownik mapujący nazwy modeli na foldery z danymi testowymi
 model_to_data_folder = {
-    # '5-layer_affectnet.keras': 'input/affectnet',
-    # '5-layer_affectnet_balanced.keras':'input/affectnet_balanced',
-	# '5-layer_fer2013.keras':'input/fer2013',
-	# '5-layer_fer2013_balanced.keras':'input/fer2013_balanced',
-	# '5-layer_merged_dataset.keras':'input/merged_dataset',
-	# '5-layer_merged_dataset_balanced.keras':'input/merged_dataset_balanced',
-	# '5-layer_mma.keras':'input/mma',
-	# '5-layer_mma_balanced.keras':'input/mma_balanced',
-	# '5-layer_fer2013_balanced.keras':'input/merged_dataset',
-	# '5-layer_affectnet_balanced.keras':'input/merged_dataset',
-	# '5-layer_mma_balanced.keras':'input/merged_dataset',
+    '5-layer_affectnet.keras':'input/affectnet',
+    '5-layer_merged_dataset.keras':'input/merged_dataset',
+    '5-layer_merged_dataset_balanced.keras':'input/merged_dataset_balanced',
+    '5-layer_balanced_filtered_FER2013.keras': 'input/balanced_filtered_FER2013',
+    '5-layer_balanced_filtered_FER2013_aug.keras': 'input/balanced_filtered_FER2013_aug',
 
-    # '5-layer_balanced_filtered_FER2013.keras': 'input/balanced_filtered_FER2013',
-    # '5-layer_balanced_filtered_FER2013.keras': 'input/merged_dataset',
-
-    # 'YT_1_balanced_filtered_FER2013.keras': 'input/balanced_filtered_FER2013',
-    # 'YT_1_balanced_filtered_FER2013.keras': 'input/merged_dataset',
-
-    # 'YT_2_balanced_filtered_FER2013.keras': 'input/balanced_filtered_FER2013',
-    # 'YT_2_balanced_filtered_FER2013.keras': 'input/merged_dataset',
-
-    # 'YT_3_balanced_filtered_FER2013.keras': 'input/balanced_filtered_FER2013',
-    # 'YT_3_balanced_filtered_FER2013.keras': 'input/merged_dataset',
-    
+	'YT_5_affectnet.keras':'input/affectnet',
+    'YT_5_merged_dataset.keras':'input/merged_dataset',
+    'YT_5_merged_dataset_balanced.keras':'input/merged_dataset_balanced',
     'YT_5_balanced_filtered_FER2013.keras': 'input/balanced_filtered_FER2013',
-
-	#'6-layer_fer2013.keras':'input/fer2013',
-	#'6-layer_mma.keras':'input/mma',
+    'YT_5_balanced_filtered_FER2013_aug.keras': 'input/balanced_filtered_FER2013_aug',
 }
 
 results = []
@@ -82,5 +66,8 @@ for model_name, data_folder in model_to_data_folder.items():
 
     # Wygenerowanie raportu klasyfikacji
     report = classification_report(y_test, y_pred, target_names=labels)
-    print(f"\n\nClassification Report for model {model_name} for {data_folder.replace('input/','')}:")
-    print(report)
+    
+    print(model_name)
+
+    print(f"\n\nClassification Report for model {model_name} for {data_folder.replace('input/','')}:", file=open(OUTPUT_FILE, 'a'))
+    print(report, file=open(OUTPUT_FILE, 'a'))
